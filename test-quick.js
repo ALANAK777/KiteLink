4c432cc959bdd70991a5082ece9fdea3f016f8ae
ALANAK777 <akhilrock638362@gmail.com>
2025-08-31 23:24:08 +0530

    Initializing project

--- test-quick.js
+++
@@ -1,1 +0,0 @@
-import { MCPClient } from "./client.js"; const client = new MCPClient(); client.connect().then(() => client.callTool("get_profile")).then(() => client.disconnect()).catch(console.error);
